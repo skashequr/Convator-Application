0debcbe158e95b29a186792af63f2a1fd1698674
skashequr <learningwithextracare@gmail.com>
2024-01-16 23:53:49 +0600

    add about page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,28 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
-import EditImg from "../Component/EditImg";
+// import EditImg from "../Component/EditImg";
 import Pdfimg from "../Component/Image to Pdf/ImageToPDF";
+import AboutUs from "../Pages/AboutUs/AboutUs";
+import MainLayout from "../MainLayout/MainLayout";
 
   export const router = createBrowserRouter([
     {
       path: "/",
-      element: <EditImg></EditImg>
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+          path: "/",
+          
+        },
+        {
+          path: "/about-us",
+          element:<AboutUs></AboutUs>
+        },
+        {
+
+        }
+      ]
     },
     {
       path: "img",
