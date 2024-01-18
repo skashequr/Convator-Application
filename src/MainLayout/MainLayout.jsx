4c9a554b5e594afa7ec1143e43e06e73d4e0c1f0
Tayeb Hossan <tayebhoss80@gmail.com>
2024-01-18 23:18:01 +0600

    nav and footer created

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../Component/NavBar/NavBar";
+import FooterR from "../Component/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet></Outlet>            
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <FooterR></FooterR>
+    </div>
+  );
 };
 
 export default MainLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
+  presets: [keepPreset],
   theme: {
     extend: {
       colors: {
@@ -29,6 +33,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 
