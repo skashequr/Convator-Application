0ea2ba83fe6b2b1c3a1148f29d7748e06ab52165
Tayeb Hossan <tayebhoss80@gmail.com>
2024-01-19 01:43:12 +0600

    banner created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,32 +1,28 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 // import EditImg from "../Component/EditImg";
 import Pdfimg from "../Component/Image to Pdf/ImageToPDF";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import MainLayout from "../MainLayout/MainLayout";
+import Home from "../Pages/Home/Home";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children:[
-        {
-          path: "/",
-          element: <h1>frtyurtfy</h1>
-          
-        },
-        {
-          path: "/about-us",
-          element:<AboutUs></AboutUs>
-        },
-        {
-
-        }
-      ]
-    },
-    {
-      path: "img",
-      element: <Pdfimg></Pdfimg>
-    }
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUs></AboutUs>,
+      },
+      {},
+    ],
+  },
+  {
+    path: "img",
+    element: <Pdfimg></Pdfimg>,
+  },
+]);
