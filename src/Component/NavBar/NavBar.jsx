66170f458444f19de15292b43a4bdfd8dd8840e0
Tayeb Hossan <tayebhoss80@gmail.com>
2024-01-24 21:49:09 +0600

    udate banner & change style nav

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   const [drawer, setDrawer] = useState(false);
 
   const navLinks = (
-   <>
+    <>
       <li>
         <NavLink
           to="/"
@@ -130,50 +130,52 @@ const NavBar = () => {
   );
 
   return (
-    <nav className="bg-purple-200 text-sm font-semibold flex px-6 sm:px-10 py-6 justify-between items-center">
-      <ul
-        className={`py-5 ${
-          drawer ? "left-0" : "-left-40"
-        } absolute  transition-all duration-300  top-20 w-36 xl:hidden flex flex-col items-center gap-6 bg-purple-200 rounded-br-2xl`}
-      >
-        {navLinks}
-        <button className="">Login</button>
-      </ul>
-      <div className="text-3xl xl:hidden flex gap-3 items-center transition-all duration-300">
-        <IoReorderThree
-          onClick={() => setDrawer(!drawer)}
-          className="text-3xl xl:hidden"
-        />
-        <img src="" alt="" />
-      </div>
-      <div className="hidden xl:inline-block">
-        <ul className="flex items-center gap-10 ">{navLinks}</ul>
-      </div>
-      <div className=" text-3xl hidden xl:inline-block">
-        <img src="https://i.ibb.co/9v8RZsr/Logo.png" alt="" />
-      </div>
-      <div className="relative flex items-center gap-5 w-fit h-fit">
-        <div className="relative flex flex-1 items-center ">
-          <input
-            className="rounded-full hidden sm:inline-block w-full h-10 pl-10 pr-2 text-gray-400 border-none"
-            placeholder="Search..."
-            type="search"
-            name=""
-            id=""
+    <div className="relative z-50 w-full">
+      <nav className="backdrop-blur-sm w-full fixed text-sm font-semibold flex px-6 sm:px-10 py-6 justify-between items-center">
+        <ul
+          className={`py-5 ${
+            drawer ? "left-0" : "-left-40"
+          } absolute  transition-all duration-300  top-20 w-36 xl:hidden flex flex-col items-center gap-6  rounded-br-2xl`}
+        >
+          {navLinks}
+          <button className="">Login</button>
+        </ul>
+        <div className="text-3xl xl:hidden flex gap-3 items-center transition-all duration-300">
+          <IoReorderThree
+            onClick={() => setDrawer(!drawer)}
+            className="text-3xl xl:hidden"
           />
-          <CiSearch className="absolute hidden sm:inline-block text-2xl left-4" />
+          <img src="" alt="" />
         </div>
-        <CiSearch className="inline-block sm:hidden text-2xl left-4" />
-        <button className="hidden lg:inline-block">
-          <Link to="/login">Login</Link>
-        </button>
-        <Link to="/signup">
-          <button className="bg-black hover:bg-zinc-700 text-nowrap text-white text-sm rounded-full px-5 py-2 ">
-            Signup Now
+        <div className="hidden xl:inline-block">
+          <ul className="flex items-center gap-10 ">{navLinks}</ul>
+        </div>
+        <div className=" text-3xl hidden xl:inline-block">
+          <img src="https://i.ibb.co/9v8RZsr/Logo.png" alt="" />
+        </div>
+        <div className="relative flex items-center gap-5 w-fit h-fit">
+          <div className="relative flex flex-1 items-center ">
+            <input
+              className="rounded-full hidden sm:inline-block w-full h-10 pl-10 pr-2 text-gray-400 border-none"
+              placeholder="Search..."
+              type="search"
+              name=""
+              id=""
+            />
+            <CiSearch className="absolute hidden sm:inline-block text-2xl left-4" />
+          </div>
+          <CiSearch className="inline-block sm:hidden text-2xl left-4" />
+          <button className="hidden lg:inline-block">
+            <Link to="/login">Login</Link>
           </button>
-        </Link>
-      </div>
-    </nav>
+          <Link to="/signup">
+            <button className="bg-black hover:bg-zinc-700 text-nowrap text-white text-sm rounded-full px-5 py-2 ">
+              Signup Now
+            </button>
+          </Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 
