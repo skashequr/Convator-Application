27c60016552c6186c6032d345e617a98eb9eb67c
skashequr <learningwithextracare@gmail.com>
2024-01-24 16:51:48 +0600

    add ui user

--- src/Pages/Massage/Massage.jsx
+++ src/Pages/Massage/Massage.jsx
@@ -4,10 +4,10 @@ const Massage = () => {
     return (
         <div>
             <div className="grid grid-col-12 bg-warning-100 h-full w-full">
-                <div className="col-span-2 h-96 bg-slate-100">
+                <div className="col-span-1 h-96 bg-slate-100">
                     <SideBar></SideBar>
                 </div>
-                <div className="col-span-8 bg-red-950">
+                <div className="col-span-12 bg-red-950">
 
                 </div>
             </div>
