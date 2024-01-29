331b64f78018301ba0853af0127bdc9bf1bcf778
Tayeb Hossan <tayebhoss80@gmail.com>
2024-01-29 14:17:34 +0600

    push for fixing some problem

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -30,12 +30,12 @@ const Banner = () => {
     "https://i.ibb.co/RgmjxFT/1.png",
     "https://i.ibb.co/rmN8qjC/3.png",
     "https://i.ibb.co/NjQmn7j/2.png",
-    "https://i.ibb.co/TwQCBgJ/4.png",
-    "https://i.ibb.co/0M0pFtS/5.png",
+    // "https://i.ibb.co/TwQCBgJ/4.png",
+    // "https://i.ibb.co/0M0pFtS/5.png",
 
-    "https://i.ibb.co/HVTx3vY/7.png",
-    "https://i.ibb.co/gyHRSX4/8.png",
-    "https://i.ibb.co/Z1v61tT/6.png",
+    // "https://i.ibb.co/HVTx3vY/7.png",
+    // "https://i.ibb.co/gyHRSX4/8.png",
+    // "https://i.ibb.co/Z1v61tT/6.png",
   ];
 
   const nextSlide = () => {
