485acfd4ec9d894900ed43e717e83b2c40199216
joy43 <ssjoy43@gmail.com>
2024-01-31 17:19:13 +0600

    some animation text to speech

--- src/Pages/Hexagon-card/multihexon/multicard.jsx
+++ src/Pages/Hexagon-card/multihexon/multicard.jsx
@@ -32,32 +32,43 @@ const MultiHexon = () => {
           </li>
         </Link>
         {/* ------------------------ yb download ------------------------ */}
-        <li className="hexagon hexagon-green">
-          <div className="hexagon-inner">
-            <img className="hexagon-avatar-img" src={imgpdf} />
-            <span className="hexagon-name">Youtube video mp3 </span>
-            <span className="hexagon-metric-label">Yb Video mp3 download</span>
-          </div>
-        </li>
+        <Link>
+          <li className="hexagon hexagon-green">
+            <div className="hexagon-inner">
+              <img className="hexagon-avatar-img" src={imgpdf} />
+              <span className="hexagon-name">Youtube video mp3 </span>
+              <span className="hexagon-metric-label">
+                Yb Video mp3 download
+              </span>
+            </div>
+          </li>
+        </Link>
+        {/* --------------voice to text----------- */}
+        <Link to="/speech">
+          <li className="hexagon hexagon-blue">
+            <div className="hexagon-inner">
+              <img className="hexagon-avatar-img" src={voicetotext} />
+              <span className="hexagon-name">Voice to text</span>
+              <span className="hexagon-metric-label">Voice convert text</span>
+            </div>
+          </li>
+        </Link>
 
-        <li className="hexagon hexagon-blue">
-          <div className="hexagon-inner">
-            <img className="hexagon-avatar-img" src={voicetotext} />
-            <span className="hexagon-name">Voice to text</span>
-            <span className="hexagon-metric-label">Voice convert text</span>
-          </div>
-        </li>
         {/* -----------------4-------------------------- */}
-        <li className="hexagon hexagon-yellow">
-          <div className="hexagon-inner">
-            <img
-              className="hexagon-avatar-img"
-              src="https://randomuser.me/api/portraits/men/32.jpg"
-            />
-            <span className="hexagon-name">Bruce Wayne</span>
-            <span className="hexagon-metric-label">Progress to Goal</span>
-          </div>
-        </li>
+        <Link to="/watermark">
+          <li className="hexagon hexagon-yellow">
+            <div className="hexagon-inner">
+              <img
+                className="hexagon-avatar-img"
+                src="https://randomuser.me/api/portraits/men/32.jpg"
+              />
+              <span className="hexagon-name">watermarked-Pdf</span>
+              <span className="hexagon-metric-label">Progress to Goal</span>
+            </div>
+          </li>
+        </Link>
+
+        {/* ---------------- */}
         <li className="hexagon hexagon-yellow">
           <div className="hexagon-inner">
             <img
