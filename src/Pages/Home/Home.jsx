78cadacd3386c38fdf92bcf95b4aa271c097be71
Tayeb Hossan <tayebhoss80@gmail.com>
2024-02-15 06:21:56 +0600

    payment method created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,12 @@ import Testimonials from "../../Component/Testemonial/Testimonials";
 import Banner from "./Banner";
 import UserStatics from "./UserStatics";
 import { Faq } from "../../Component/FAQ/Faq";
-import MultiHexon from "../Hexagon-card/multihexon/multicard";import { useContext } from "react";
+import MultiHexon from "../Hexagon-card/multihexon/multicard";
+import { useContext } from "react";
 import { AuthContext } from "../Authentication/AuthProvider/Authprovider";
+import AccessCard from "../../Component/AccessCard/AccessCard";
 
 const Home = () => {
-  
   return (
     <div>
       <div>
@@ -18,6 +19,7 @@ const Home = () => {
         <UserStatics></UserStatics>
         <MultiHexon></MultiHexon>
         <Testimonials></Testimonials>
+        <AccessCard></AccessCard>
         <Faq></Faq>
       </div>
     </div>
